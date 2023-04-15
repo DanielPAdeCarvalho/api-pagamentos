6aecbf6b9c287ad94f1c4bbfd46bc95234016750
Daniel Carvalho <129884281+DanielPAdeCarvalho@users.noreply.github.com>
2023-04-15 16:39:16 -0300

    introduzir o campo pagamentos

--- models/models.go
+++ models/models.go
@@ -1,13 +1,20 @@
 package models
 
 type Pagamento struct {
-	Cliente    string  `json:"cliente"`
-	Troco      float64 `json:"troco"`
-	Credito    float64 `json:"credito"`
-	Debito     float64 `json:"debito"`
-	Dinheiro   float64 `json:"dinheiro"`
-	Picpay     float64 `json:"picpay"`
-	Pix        float64 `json:"pix"`
-	Persycoins float64 `json:"persycoins"`
-	Data       string  `json:"data"`
+	Cliente    string            `json:"cliente"`
+	Troco      float64           `json:"troco"`
+	Credito    float64           `json:"credito"`
+	Debito     float64           `json:"debito"`
+	Dinheiro   float64           `json:"dinheiro"`
+	Picpay     float64           `json:"picpay"`
+	Pix        float64           `json:"pix"`
+	Persycoins float64           `json:"persycoins"`
+	Data       string            `json:"data"`
+	Pedidos    map[string]Pedido `json:"pedidos"`
+}
+
+type Pedido struct {
+	Nome       string  `json:"nome"`
+	Preco      float64 `json:"preco"`
+	Quantidade int     `json:"quantidade"`
 }
